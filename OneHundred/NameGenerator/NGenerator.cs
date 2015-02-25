ee9bbe1dddc40da200751895299864f37982c215
Jasak <efektmotyla@gmail.com>
2015-02-25 09:34:19 +0100

    Added OneHundredWeb project. First concept of an console app web window.

---
+++ OneHundred/NameGenerator/NGenerator.cs
@@ -0,0 +1,67 @@
+﻿using CommonCode;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameGenerator
+{
+    public class NGenerator : IProgram
+    {
+        Random random;
+
+        private readonly string[] C = new string[] { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z" };
+        private string[] V = new string[] { "a", "i", "e", "o", "u" };
+        private string[] Ending = new string[] { "ith", "ton", "on", "field", "man" };
+
+        private List<List<string[]>> Grammars;
+
+        public NGenerator()
+        {
+            random = new Random(DateTime.Now.Millisecond);
+
+            Grammars = new List<List<string[]>>() {
+                new List<string[]>() { C, V, Ending },
+                new List<string[]>() { C, V, C, V, Ending }
+            };
+        }
+
+        public void Run()
+        {
+            ConsoleKeyInfo userInput;
+            Console.Clear();
+            Console.WriteLine("Hit Escape to stop. Anykey will continue generation.");
+            do
+            {
+                Console.WriteLine(Generate());
+                userInput = Console.ReadKey();
+            }
+            while (userInput.Key != ConsoleKey.Escape);
+            
+        }
+
+        public string Generate()
+        {
+            StringBuilder nameBuilder = new StringBuilder();
+
+            var grammar = PickRandom<List<string[]>>(Grammars);
+
+            for (int i = 0; i < grammar.Count; i++)
+            {
+                if (i == 0)
+                    nameBuilder.Append(PickRandom<string>(grammar[i]).ToUpper());
+                else
+                    nameBuilder.Append(PickRandom<string>(grammar[i]));
+            }
+
+            return nameBuilder.ToString();
+        }
+
+        private T PickRandom<T>(IEnumerable<T> array)
+        {
+            return array.ToArray()[random.Next(array.ToArray().Length)];
+        }
+    }
+}
---
+++ OneHundred/OneHundredWeb/Areas/Console/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OneHundredWeb.Areas.Console.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<string> lines = new List<string>();
+
+        public ActionResult Index()
+        {
+            ProcessStartInfo processStartInfo =
+                new ProcessStartInfo(@"C:\Projects\Git\OneHundred\OneHundred\ConsoleProgramLuncher\bin\Debug\ConsoleProgramLuncher.exe");
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.ErrorDialog = false;
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+            
+            Process process = new Process();
+
+            process.EnableRaisingEvents = true;
+            process.ErrorDataReceived += process_ErrorDataReceived;
+            process.OutputDataReceived += process_OutputDataReceived;
+
+            process.StartInfo = processStartInfo;
+            process.Start();
+
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+
+            process.StandardInput.Write("e");
+            process.StandardInput.Flush();
+
+            process.WaitForExit();
+
+            ViewBag.Lines = lines;
+
+            return View();
+        }
+
+        void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(e.Data))
+                lines.Add(e.Data);
+        }
+
+        void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(e.Data))
+                lines.Add(String.Format("Error: {0}", e.Data));
+        }
+    }
+}
